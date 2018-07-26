6f3f752b6a20f7d53089dc32cc819dab6150098e
Sarah-Marion <devsarahmarion@gmail.com>
2018-07-26 09:01:02 +0300

    ordering the projects by id

--- cv/models.py
+++ cv/models.py
@@ -15,7 +15,7 @@ class Project(models.Model):
     project_description = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
-        return str(self.id)
+        return str(self.project_name)
     class Meta:
         ordering = ('id',)
 
